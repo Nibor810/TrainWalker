58eb8c7bfd2a798fcf09156100e5068bb8b4f766
Unknown <rm.hobbel@telfort.nl>
2017-12-15 11:13:42 +0100

    Added newInstance Method

--- app/src/main/java/com/example/robin/trainwalker/FavoriteTrainPopupFragment.java
+++ app/src/main/java/com/example/robin/trainwalker/FavoriteTrainPopupFragment.java
@@ -1,5 +1,6 @@
 package com.example.robin.trainwalker;
 
+import android.app.DialogFragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -19,6 +20,11 @@ public class FavoriteTrainPopupFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_favorite_train_popup, container, false);
+    }
+
+    public static FavoriteTrainPopupFragment newInstance() {
+        FavoriteTrainPopupFragment fragment = new FavoriteTrainPopupFragment();
+        return fragment;
     }
 
 
--- app/src/main/java/com/example/robin/trainwalker/SettingsFragment.java
+++ app/src/main/java/com/example/robin/trainwalker/SettingsFragment.java
@@ -1,16 +1,15 @@
 package com.example.robin.trainwalker;
 
-import android.content.Context;
-import android.content.DialogInterface;
-import android.net.Uri;
+
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.Gravity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.PopupWindow;
+
 
 public class SettingsFragment extends Fragment {
     Button openPopupButton;
@@ -23,23 +22,19 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        openPopupButton = getActivity().findViewById(R.id.settings_selectFavoriteTrainButton);
-        openPopupButton.setOnClickListener(v->{
-            showPopup();
-        });
-
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        openPopupButton = view.findViewById(R.id.settings_selectFavoriteTrainButton);
+        openPopupButton.setOnClickListener(v->{
+            showPopup();
+        });
+        return view;
     }
 
     private void showPopup(){
         //TODO: Open Popup
     }
-
-
-
 }
