55fe62a67176dd1bc73711b15f2bfbe710012fa4
Unknown <rm.hobbel@telfort.nl>
2017-12-15 10:38:40 +0100

    Added Fragments To navigation Bar, navigation bar now works

--- app/src/main/java/com/example/robin/trainwalker/MainActivity.java
+++ app/src/main/java/com/example/robin/trainwalker/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.robin.trainwalker;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -16,18 +18,25 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    selectedFragment = HomeFragment.newInstance();
+                    break;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
+                    selectedFragment = HomeFragment.newInstance();
+                    break;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                    selectedFragment = HomeFragment.newInstance();
+                    break;
+                case R.id.navigation_settings:
+                    selectedFragment = SettingsFragment.newInstance();
+                    break;
             }
-            return false;
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.main_frame,selectedFragment);
+            transaction.commit();
+            return true;
         }
     };
 
@@ -36,9 +45,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_frame,HomeFragment.newInstance());
+        transaction.commit();
     }
 
 }
