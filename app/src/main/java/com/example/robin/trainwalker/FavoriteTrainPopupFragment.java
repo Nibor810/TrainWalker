37f3f02380427381350713b2bfce08808d20b2c9
Unknown <rm.hobbel@telfort.nl>
2017-12-15 10:14:52 +0100

    Added Popup XML

---
+++ app/src/main/java/com/example/robin/trainwalker/FavoriteTrainPopupFragment.java
@@ -0,0 +1,25 @@
+package com.example.robin.trainwalker;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by robin on 15-12-2017.
+ */
+
+public class FavoriteTrainPopupFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_favorite_train_popup, container, false);
+    }
+
+
+}
