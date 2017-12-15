81ae13e9228acac934a36e987f937301e1b3fa66
Unknown <rm.hobbel@telfort.nl>
2017-12-15 10:55:06 +0100

    Updated To 1.8 Java

--- app/src/main/java/com/example/robin/trainwalker/SettingsFragment.java
+++ app/src/main/java/com/example/robin/trainwalker/SettingsFragment.java
@@ -1,14 +1,20 @@
 package com.example.robin.trainwalker;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
 
 public class SettingsFragment extends Fragment {
+    Button openPopupButton;
+
     public static SettingsFragment newInstance() {
         SettingsFragment fragment = new SettingsFragment();
         return fragment;
@@ -17,11 +23,21 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        openPopupButton = getActivity().findViewById(R.id.settings_selectFavoriteTrainButton);
+        
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
+
+    private void showPopup(){
+
+    }
+
+
 
 }
