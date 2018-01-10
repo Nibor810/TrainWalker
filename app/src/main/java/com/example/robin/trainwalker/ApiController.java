8c9beb708efaf500ed5ddcc571e3e5fcc1c8234f
ArthurvanStrien <arthurvanstrien@gmail.com>
2018-01-10 15:39:06 +0100

    Created an inner class that handles the stations request.

--- app/src/main/java/com/example/robin/trainwalker/ApiController.java
+++ app/src/main/java/com/example/robin/trainwalker/ApiController.java
@@ -9,9 +9,29 @@ public class ApiController
     String username;
     String password;
 
-    public ApiController(String username, String password)
+    public ApiController()
     {
-        this.username = username;
-        this.password = password;
+        username = "";
+        password = "";
+    }
+
+    public void requestStations()
+    {
+        new RequestStations();
+    }
+
+    private class RequestStations implements AsyncResponse
+    {
+        public RequestStations()
+        {
+            String url = "http://webservices.ns.nl/ns-api-stations-v2";
+            new RequestController(username, password, url, this).execute();
+        }
+
+        @Override
+        public void processFinished(String result)
+        {
+            System.out.println(result);
+        }
     }
 }
