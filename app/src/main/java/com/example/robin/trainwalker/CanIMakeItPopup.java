9bb5a1494d22ffd7546b01f11ee149cb26ae2f86
Unknown <rm.hobbel@telfort.nl>
2018-01-12 11:18:34 +0100

    added callback to button. popup now goes to mapfragment after confirmation

---
+++ app/src/main/java/com/example/robin/trainwalker/CanIMakeItPopup.java
@@ -0,0 +1,51 @@
+package com.example.robin.trainwalker;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by robin on 12-1-2018.
+ */
+
+public class CanIMakeItPopup extends Dialog {
+    Button cancel;
+    Button accept;
+    TextView textViewTime;
+    PopUpCallBack callBack;
+
+    public CanIMakeItPopup(@NonNull Context context, PopUpCallBack callBack) {
+        super(context);
+        this.callBack = callBack;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.fragment_popup_can_i_make_it);
+        cancel = findViewById(R.id.popup_makeit_cancelButton);
+        accept = findViewById(R.id.popup_makeit_continueButton);
+        textViewTime = findViewById(R.id.popup_makeit_textViewTime);
+        cancel.setOnClickListener(view -> {
+            dismiss();
+        });
+        accept.setOnClickListener(view -> {
+            goToMapFragment();
+        });
+    }
+
+    private void goToMapFragment() {
+        callBack.doAfterPopup();
+        dismiss();
+    }
+
+
+}
--- app/src/main/java/com/example/robin/trainwalker/TrainFragment.java
+++ app/src/main/java/com/example/robin/trainwalker/TrainFragment.java
@@ -3,16 +3,20 @@ package com.example.robin.trainwalker;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TrainFragment extends Fragment {
+public class TrainFragment extends Fragment implements PopUpCallBack {
+    Button chooseStationsButton;
+
     public TrainFragment() {
     }
 
@@ -30,7 +34,26 @@ public class TrainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_train, container, false);
-
+        chooseStationsButton = view.findViewById(R.id.train_chooseStationsButton);
+        chooseStationsButton.setOnClickListener(view1 -> {
+            showPopup();
+        });
         return view;
+    }
+
+    private void showPopup(){
+        CanIMakeItPopup customDialog =new CanIMakeItPopup(this.getContext(),this);
+        customDialog.show();
+    }
+
+    private void goToMapFragment(){
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_frame,MapFragment.newInstance());
+        transaction.commit();
+    }
+
+    @Override
+    public void doAfterPopup() {
+        goToMapFragment();
     }
 }
