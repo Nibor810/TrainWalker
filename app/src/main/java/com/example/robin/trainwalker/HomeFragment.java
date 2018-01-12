05830c003f1c6ac595f0aefc454f7f2a18987d74
Unknown <rm.hobbel@telfort.nl>
2018-01-12 13:00:12 +0100

    Finished functionality for home screen

--- app/src/main/java/com/example/robin/trainwalker/HomeFragment.java
+++ app/src/main/java/com/example/robin/trainwalker/HomeFragment.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements PopUpCallBack {
     Button favoriteTrainButton;
     Button differentTrainButton;
     BottomNavigationView navigation;
@@ -45,7 +45,17 @@ public class HomeFragment extends Fragment {
     }
 
     private void onFavoriteTrainButtonClicked() {
-        
+        if(hasFavoriteTrain())
+            showPopup();
+    }
+
+    private void showPopup(){
+        CanIMakeItPopup customDialog = new CanIMakeItPopup(this.getContext(),this);
+        customDialog.show();
+    }
+
+    private boolean hasFavoriteTrain() {
+        return true;
     }
 
     private void goToTrainFragment(){
@@ -53,5 +63,17 @@ public class HomeFragment extends Fragment {
         transaction.replace(R.id.main_frame,TrainFragment.newInstance());
         transaction.commit();
         navigation.setSelectedItemId(R.id.navigation_trains);
+    }
+
+    private void goToMapFragment(){
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_frame,MapFragment.newInstance());
+        transaction.commit();
+        navigation.setSelectedItemId(R.id.navigation_map);
+    }
+
+    @Override
+    public void doAfterPopup() {
+        goToMapFragment();
     }
 }
