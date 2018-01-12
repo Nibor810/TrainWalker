c0234f144701ad1b9ed02e0cc7d7792ccfe2db2b
ArthurvanStrien <arthurvanstrien@gmail.com>
2018-01-12 13:41:39 +0100

    Refactored the layout of the ApiController

--- app/src/main/java/com/example/robin/trainwalker/ApiController.java
+++ app/src/main/java/com/example/robin/trainwalker/ApiController.java
@@ -4,33 +4,32 @@ package com.example.robin.trainwalker;
  * Created by Arthur on 16-12-2017.
  */
 
-public class ApiController
-{
-    String username;
-    String password;
+public class ApiController {
 
-    public ApiController()
-    {
-        username = "";
-        password = "";
+    private String username;
+    private String password;
+
+    public ApiController() {
+
+
     }
 
-    public void requestStations()
-    {
+    public void requestStations() {
+
         new RequestStations();
     }
 
-    private class RequestStations implements AsyncResponse
-    {
-        public RequestStations()
-        {
+    private class RequestStations implements AsyncResponse {
+
+        public RequestStations() {
+
             String urlString = "http://webservices.ns.nl/ns-api-stations-v2";
             new RequestController(username, password, urlString, this).execute();
         }
 
         @Override
-        public void processFinished(String result)
-        {
+        public void processFinished(String result) {
+
             System.out.println(result);
         }
     }
