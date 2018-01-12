bda64464a7245b4099de3934b0fb1b2061ba1c67
Unknown <rm.hobbel@telfort.nl>
2018-01-12 14:01:36 +0100

    Added Stringbuilder and some base methods

--- app/src/main/java/com/example/robin/trainwalker/MapFragment.java
+++ app/src/main/java/com/example/robin/trainwalker/MapFragment.java
@@ -1,18 +1,44 @@
 package com.example.robin.trainwalker;
 
+import android.app.DownloadManager;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback{
-
-    GoogleMap map;
+    public final static String KEY_ROUTE = "ROUTE";
+    private final static int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0;
+    private final static float DEFAULT_ZOOM = 18f;
+    private final static String KEY_LOCATION = "LOCATION";
+    private final static String KEY_CAMERA_POSITION = "CAMERA_POSITION";
+    private final static int ZOOM_THRESHOLD = 10;
+    private Location lastKnownLocation = null;
+    private CameraPosition cameraPosition = null;
+    private GoogleMap map;
+    private GoogleApiClient googleApiClient;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LatLng defaultLocation = new LatLng(0, 0);
 
     public MapFragment() {
         // Required empty public constructor
@@ -45,4 +71,83 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
     }
+
+    private void getDeviceLocation() {
+        try {
+            if (hasLocationPermission()) {
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this.getActivity(), (task) -> {
+                    if (task.isSuccessful()) {
+                        lastKnownLocation = task.getResult();
+                        if (cameraPosition == null && lastKnownLocation != null)
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(lastKnownLocation.getLatitude(),
+                                            lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                    } else {
+                        if (cameraPosition == null)
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
+                        map.getUiSettings().setMyLocationButtonEnabled(false);
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    private boolean hasLocationPermission() {
+        return ContextCompat.checkSelfPermission(this.getActivity().getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void getLocationPermission() {
+        if (!hasLocationPermission()) {
+            ActivityCompat.requestPermissions(this.getActivity(),
+                    new String[]{(android.Manifest.permission.ACCESS_FINE_LOCATION)},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    private void updateLocationUI() {
+        try {
+            if (hasLocationPermission()) {
+                map.setMyLocationEnabled(true);
+                map.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                map.setMyLocationEnabled(false);
+                map.getUiSettings().setMyLocationButtonEnabled(false);
+                lastKnownLocation = null;
+            }
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    private String createRouteURL(LatLng ownLocation,LatLng stationLocation) {
+        // Origin of route
+        LatLng originLatLng;
+        String str_origin;
+
+        // Detination of route
+        LatLng destLatLng;
+        String str_dest;
+
+        // Mode of transportation
+        String trafficMode = "mode=walking";
+
+        // Waypoints of route
+        originLatLng = ownLocation;
+        str_origin = "origin=" + originLatLng.latitude + "," + originLatLng.longitude;
+
+        destLatLng = stationLocation;
+        str_dest = "destination=" + destLatLng.latitude + "," + destLatLng.longitude;
+
+        // Url building
+        String parameters = str_origin + "&" + str_dest + "&" + trafficMode;
+
+        String output = "json";
+
+        return "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters + "&key=" + getString(R.string.google_maps_key);
+    }
+
 }
