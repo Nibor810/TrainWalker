8ce814beb899e7b94da7b478e4077c90022292fb
ArthurvanStrien <arthurvanstrien@gmail.com>
2018-01-12 11:30:49 +0100

    Added the onclick listener tothe favoriteTrainButton

--- app/src/main/java/com/example/robin/trainwalker/HomeFragment.java
+++ app/src/main/java/com/example/robin/trainwalker/HomeFragment.java
@@ -7,12 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.google.android.gms.common.api.Api;
-
 public class HomeFragment extends Fragment {
 
     private ApiController apiController;
     private Button favoriteTrainButton;
+    private Button otherTrainButton;
 
     public static HomeFragment newInstance() {
         HomeFragment fragment = new HomeFragment();
@@ -32,11 +31,17 @@ public class HomeFragment extends Fragment {
         apiController = new ApiController();
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        favoriteTrainButton = view.findViewById(R.id.home_FavoriteTrainButton);
 
-        favoriteTrainButton.setOnClickListener(v->
-        {
+        favoriteTrainButton = view.findViewById(R.id.home_FavoriteTrainButton);
+        favoriteTrainButton.setOnClickListener(v-> {
+
             //apiController.requestStations();
+        });
+
+        otherTrainButton = view.findViewById(R.id.home_OtherTrainButton);
+        otherTrainButton.setOnClickListener(v -> {
+
+            
         });
 
         return view;
