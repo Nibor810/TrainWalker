a72e8ca2eb42149916a8d962bc9b40a39756b744
ArthurvanStrien <arthurvanstrien@gmail.com>
2018-01-12 14:00:38 +0100

    Added a request to the ApiController that requests the train departure times of the given station.

--- app/src/main/java/com/example/robin/trainwalker/ApiController.java
+++ app/src/main/java/com/example/robin/trainwalker/ApiController.java
@@ -19,6 +19,21 @@ public class ApiController {
         new RequestStations();
     }
 
+    /**
+     * Request the departing trains of a given station.
+     * The request will return at least 10 departing trains.
+     * The request will return at least all departing trains of the coming hour.
+     * This means that at a small station where a train stops every half hour, the departing trains-
+     * for the coming five hours are returned to meet the minimum of 10 departing trains.
+     * This also means that at a busy station where a train departes every 5 minutes, the return -
+     * list contains 12 trains to meet the minimum of an hour of trains.
+     * @param stationName
+     */
+    public void requestStationDepartureTime(String stationName) {
+
+        new RequestStationDepartureTimes(stationName);
+    }
+
     private class RequestStations implements AsyncResponse {
 
         public RequestStations() {
@@ -30,6 +45,23 @@ public class ApiController {
         @Override
         public void processFinished(String result) {
 
+            //TODO PARSE RESULT.
+            System.out.println(result);
+        }
+    }
+
+    private class RequestStationDepartureTimes implements AsyncResponse {
+
+        public RequestStationDepartureTimes(String stationName) {
+
+            String urlString = "https://webservices.ns.nl/ns-api-avt?station=" + stationName;
+            new RequestController(username, password, urlString, this).execute();
+        }
+
+        @Override
+        public void processFinished(String result) {
+
+            //TODO PARSE RESULT.
             System.out.println(result);
         }
     }
