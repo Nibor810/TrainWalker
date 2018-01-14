6ec384a20103e8f04a3e71fda74eff919f32cdd6
ArthurvanStrien <arthurvanstrien@gmail.com>
2018-01-14 12:34:59 +0100

    Added a method to the DR Api request parser to parse the station request with.

--- app/src/main/java/com/example/robin/trainwalker/DRApiResponseParser.java
+++ app/src/main/java/com/example/robin/trainwalker/DRApiResponseParser.java
@@ -36,4 +36,79 @@ public class DRApiResponseParser {
             e.printStackTrace();
         }
     }
+
+    public List<Station> parseStationRequest(String responseString) {
+
+        Log.d("MESSAGE", "Parsing Station Request");
+
+        List<Station> stations = new ArrayList<>();
+
+        try {
+
+            xmlPullParser.setInput(new StringReader(responseString));
+            int eventType = xmlPullParser.getEventType();
+            int i = 0;
+
+            while(eventType != XmlPullParser.END_DOCUMENT) {
+
+                if(eventType == XmlPullParser.START_TAG && xmlPullParser.getName().equals("Station")) {
+
+                    String stationName = "";
+                    double latitude = 0;
+                    double longitude = 0;
+
+                    while (!(eventType == XmlPullParser.END_TAG && xmlPullParser.getName().equals("Station"))) {
+
+                        if(eventType == XmlPullParser.START_TAG) {
+
+                            //Loop through the names and retrieve the name with the tag "long - lang".
+                            if(xmlPullParser.getName().equals("Namen")) {
+
+                                while(!(eventType == XmlPullParser.END_TAG && xmlPullParser.getName().equals("Namen"))) {
+
+                                    if(eventType == XmlPullParser.START_TAG && xmlPullParser.getName().equals("Lang")) {
+
+                                        //Skip to the content of the Lang tag.
+                                        xmlPullParser.next();
+                                        stationName = xmlPullParser.getText();
+                                    }
+
+                                    eventType = xmlPullParser.next();
+                                }
+                            }
+                            else if(xmlPullParser.getName().equals("Lat")) {
+
+                                //Skip to the content of the Lat tag.
+                                xmlPullParser.next();
+                                latitude = Double.parseDouble(xmlPullParser.getText());
+                            }
+                            else if(xmlPullParser.getName().equals("Lon")) {
+
+                                //Skip to the content of the Lon tag.
+                                xmlPullParser.next();
+                                longitude = Double.parseDouble(xmlPullParser.getText());
+                            }
+                        }
+
+                        eventType = xmlPullParser.next();
+                    }
+
+                    stations.add(new Station(stationName, new GeoCoordinate(latitude, longitude)));
+                }
+
+                eventType = xmlPullParser.next();
+            }
+
+        }
+        catch (XmlPullParserException e) {
+
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+        return stations;
+    }
 }
